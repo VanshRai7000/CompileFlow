0c9946523ca3687d41e9c400e9adc6115d77b2fe
Vansh Rai Chaudhary <chaudhrayvansh@gamil.com>
2026-03-29 21:12:28 +0530

    Set-up for Desktopm app using Electron

---
+++ main.js
@@ -0,0 +1,30 @@
+import { app, BrowserWindow } from 'electron';
+import process from 'process';
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+
+  // Load React app
+  mainWindow.loadURL('http://localhost:5173');
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
